516921b910b6986e8ddbfcb652b7c433c5c0c971
doomed-phobos <the_ekj.2121@outlook.com>
2023-09-29 08:42:09 -0500

    Added Function
    * Added function view
    * Added benchmark to test Function and std::function

---
+++ src/menu/function.hpp
@@ -0,0 +1,55 @@
+// https://vittorioromeo.info/index/blog/passing_functions_to_functions.html
+
+#pragma once
+// #include <concepts>
+#include <type_traits>
+#include <utility>
+
+namespace menu::priv {
+   template<typename Signature>
+   class Function {};
+
+   template<typename R, typename... Args>
+   class Function<R(Args...)> {
+      typedef R(*callback_t)(const void*, Args&&...);
+      typedef std::aligned_union_t<8, void*, R(*)(Args...)> storage_t;
+      
+      storage_t m_storage;
+      callback_t m_callback;
+
+      void* get_memory() {
+         return &m_storage;
+      }
+      const void* get_memory() const {
+         return &m_storage;
+      }
+   public:
+      explicit Function(R(*fn)(Args...)) {
+         using pointer_t = R(*)(Args...);
+
+         ::new (get_memory()) pointer_t(fn);
+
+         m_callback = [](const void* ptr, Args&&... args) {
+            auto func = *static_cast<const pointer_t*>(ptr);
+            return func(std::forward<Args>(args)...);
+         };
+      }
+
+      template<typename Fn>
+      explicit Function(Fn& fn) {
+         using pointer_t = void*;
+
+         ::new (get_memory()) pointer_t(&fn);
+
+         m_callback = [](const void* _ptr, Args&&... args) {
+            auto ptr = *static_cast<const pointer_t*>(_ptr);
+            auto& func = *static_cast<Fn*>(ptr);
+            return static_cast<R>(func(std::forward<Args>(args)...));
+         };
+      }
+
+      R operator()(Args&&... args) const {
+         return m_callback(get_memory(), std::forward<Args>(args)...);
+      }
+   };   
+} // namespace menu::priv
---
+++ test/function.cc
@@ -0,0 +1,43 @@
+#include <benchmark/benchmark.h>
+#include <functional>
+#include <menu/function.hpp>
+
+static void foo() {
+   for(int i = 0; i < 10; ++i);
+}
+
+static void STDFunction_Lambda(benchmark::State& state) {
+   for(auto _ : state) {
+      auto bar = [&]() {};
+      std::function f(bar);
+      f();
+   }
+}
+
+static void STDFunction_Function(benchmark::State& state) {
+   for(auto _ : state) {
+      std::function f{foo};
+      f();
+   }
+}
+
+static void MENUFunction_Lambda(benchmark::State& state) {
+   for(auto _ : state) {
+      auto bar = [&]() {};
+      menu::priv::Function<void()> f(bar);
+      f();
+   }
+}
+
+static void MENUFunction_Function(benchmark::State& state) {
+   for(auto _ : state) {
+      menu::priv::Function<void()> f(foo);
+      f();
+   }
+}
+
+BENCHMARK(STDFunction_Lambda);
+BENCHMARK(STDFunction_Function);
+BENCHMARK(MENUFunction_Lambda);
+BENCHMARK(MENUFunction_Function);
+BENCHMARK_MAIN();
